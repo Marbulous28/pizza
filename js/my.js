b4048f3475f3d6517edaf4a42bac19db10e74cfa
Peter Beach <peterbeach2@gmail.com>
2016-04-08 13:40:10 -0700

    cost now displays

--- js/my.js
+++ js/my.js
@@ -8,12 +8,12 @@ $(document).ready(function() {
                                 '<fieldset class="form-group"> ' +
                                   '<label for="topping-select">Select a topping</label>' +
                                   '<select class="toppings form-control">' +
-                                    '<option value="mushroom">Mushrooms</option>' +
-                                    '<option value="onion">Onions</option>' +
-                                    '<option value="pepper">Peppers</option>' +
+                                    '<option value="mushrooms">Mushrooms</option>' +
+                                    '<option value="onions">Onions</option>' +
+                                    '<option value="peppers">Peppers</option>' +
                                     '<option value="pineapple">Pineapple</option>' +
-                                    '<option value="tomato">Tomatoes</option>' +
-                                    '<option value="olive">Olives</option>' +
+                                    '<option value="tomatoes">Tomatoes</option>' +
+                                    '<option value="olives">Olives</option>' +
                                     '<option value="sausage">Sausage</option>' +
                                     '<option value="pepperoni">Pepperoni</option>' +
                                   '</select>' +
@@ -27,13 +27,15 @@ $(document).ready(function() {
 
     var inputtedSize = $("#size").val();
     var orderedPizza = new Pizza(inputtedSize)
+    var length = null;
 
     $(".new-topping").each(function() {
       var inputtedToppings = $(this).find(".toppings").val();
-      orderedPizza.AddToppings(inputtedToppings);
+      length = orderedPizza.AddToppings(inputtedToppings);
     });
 
-    $("ul#display-order").append("<li>" + orderedPizza.display() + "</li>")
+    $("div#display-order").append("<p>" + orderedPizza.Display() + "</p>")
+    $("div#display-order").append("<p>" + orderedPizza.ThatWillCost(length) + "</p>")
   });
 
 
@@ -47,8 +49,22 @@ function Pizza(size) {
 
 Pizza.prototype.AddToppings = function(toppingsToBeAdded) {
   this.toppings.push(toppingsToBeAdded);
+  return this.toppings.length;
 }
 
-Pizza.prototype.display = function() {
-  return "A " + this.size + " pizza with : " + this.toppings;
+
+Pizza.prototype.Display = function() {
+  return "One " + this.size + " pizza with : " + this.toppings.join(", ");
+}
+
+Pizza.prototype.ThatWillCost = function(numberToppings) {
+  if (this.size == "small") {
+      return "That will be " + (numberToppings + 10) + "$ in total";
+  } else if (this.size == "medium") {
+      return "That will be " + (numberToppings + 12) + "$ in total";
+  } else if (this.size == "large") {
+      return "That will be " + (numberToppings + 14) + "$ in total";
+  } else if (this.size == "xl") {
+      return "That will be " + (numberToppings + 16) + "$ in total";
+  }
 }
